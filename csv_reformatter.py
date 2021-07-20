39ebb3e9f7d27984c839c1c824e775a70c40985a
acetone415 <acetone415@gmail.com>
2021-07-20 19:27:13 +0300

    add directories to store input and output reports

--- csv_reformatter.py
+++ csv_reformatter.py
@@ -3,22 +3,27 @@ import datetime
 from collections import namedtuple
 from pathlib import Path
 
-
-DIR_TO_REFORMAT = Path(__file__) / 'reports/to_reformat'
-DIR_REFORMATTED = Path(__file__) / 'reports/reformatted'
-
+# Directories with csv files to reformat and reformatted files
+DIR_TO_REFORMAT = Path(__file__).parent / 'reports/to_reformat'
+DIR_REFORMATTED = Path(__file__).parent / 'reports/reformatted'
 
 start_time = datetime.datetime.now()
 
-filename = 'report_example2'  # input file
-reformatted_csv = f'{filename}_made_on_{datetime.datetime.now().date()}'  # output file
+# Input filename
+filename = 'report_example2.csv'
+file_path = DIR_TO_REFORMAT / filename
+
+# Output file
+reformatted_csv = f'{datetime.datetime.now().date()}_{filename}'
+reformatted_csv_filepath = DIR_REFORMATTED / reformatted_csv
 
 keys = ('office', 'id', 'full_name', 'reg_numb', 'date')  # input file columns
 Person = namedtuple('Person', keys)
 info = {}
 all_docs_list = []  # list of documents to sign
 
-with open(f'{filename}.csv', encoding='utf_8', newline='\n') as csvfile:
+
+with open(file_path, encoding='utf_8', newline='\n') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
         employee = Person(*row)
@@ -33,7 +38,8 @@ with open(f'{filename}.csv', encoding='utf_8', newline='\n') as csvfile:
         if employee.reg_numb not in all_docs_list:
             all_docs_list.append(employee.reg_numb)
 
-with open(f'{reformatted_csv}.csv', 'w', encoding='utf-8', newline='\n') as csvfile:
+
+with open(reformatted_csv_filepath, 'w', encoding='utf-8', newline='\n') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['Филиал', 'ФИО', 'ТН'] + all_docs_list)
     for item in info:
@@ -46,4 +52,5 @@ with open(f'{reformatted_csv}.csv', 'w', encoding='utf-8', newline='\n') as csvf
                          *doc_field
                          ]
                         )
+
 print(f'Time:{datetime.datetime.now() - start_time}')
